145cf77dc1f029efa32d53b415eaef2280cc744d
kuuwannya <kuuuuu05062525@gmail.com>
2022-08-26 21:34:12 +0900

    add: incomesテーブルとstatusテーブルのunique制約を追加 #2

--- app/tokyoto_app/db/migrate/20220826004956_create_statuses.rb
+++ app/tokyoto_app/db/migrate/20220826004956_create_statuses.rb
@@ -1,7 +1,7 @@
 class CreateStatuses < ActiveRecord::Migration[6.0]
   def change
     create_table :statuses do |t|
-      t.integer :status, null: false, default: 0
+      t.integer :status, null: false, unique: true, default: 0
 
       t.timestamps
     end

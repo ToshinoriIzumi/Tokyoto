120c13c7ca9fe3167670df7ee08d6a9fa642c0a1
ushijima eisaku <eisakuushijima@gmail.com>
2022-08-27 11:56:04 +0900

    add ビューの追加

--- app/tokyoto_app/app/controllers/top_pages_controller.rb
+++ app/tokyoto_app/app/controllers/top_pages_controller.rb
@@ -1,4 +1,17 @@
 class TopPagesController < ApplicationController
+  before_action :set_q, only: [:index, :search]
+
   def main
   end
+
+  def search
+    @results = @q.result
+  end
+
+  private
+
+  def set_q
+    @q = Support.ransack(params[:q])
+  end
+
 end

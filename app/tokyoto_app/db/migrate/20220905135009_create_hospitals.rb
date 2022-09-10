780abbd0f75b46160eb48e705313adfe2b55d936
izumi.toshinori <toshinori622@gmail.com>
2022-09-10 14:15:05 +0900

    migrateファイルの作成

--- app/tokyoto_app/db/migrate/20220905135009_create_hospitals.rb
+++ app/tokyoto_app/db/migrate/20220905135009_create_hospitals.rb
@@ -1,8 +1,11 @@
 class CreateHospitals < ActiveRecord::Migration[6.0]
   def change
     create_table :hospitals do |t|
-      
       t.references :city, null: false, foreign_key: true
+      t.string :name, null: false
+      t.float :latitude, null: false
+      t.float :longitude, null: false
+      t.string :address, null: false
       t.timestamps
     end
   end

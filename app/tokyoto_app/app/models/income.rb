f8a12c9ddd960d090182b5b8c0a6f52140eb1c27
yutitakagi <takagiyuri0712@gmail.com>
2022-09-14 10:31:46 +0900

    change: income.rbの本人フラグのバリデーションを削除

--- app/tokyoto_app/app/models/income.rb
+++ app/tokyoto_app/app/models/income.rb
@@ -3,6 +3,6 @@ class Income < ApplicationRecord
   has_many :conditions_supports, dependent: :destroy
 
   validates :money, uniqueness: true
-  validates :is_myself
+
   enum is_myself: { user: 0, other: 1 , household: 2 }
 end

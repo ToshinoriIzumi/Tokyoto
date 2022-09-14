a09e4bf41b05d5a25ee4a02b851e889134f223a6
yutitakagi <takagiyuri0712@gmail.com>
2022-09-14 10:19:54 +0900

    change: 所得額テーブル内ではnullを許容するように変更

--- app/tokyoto_app/app/models/income.rb
+++ app/tokyoto_app/app/models/income.rb
@@ -2,7 +2,7 @@
 class Income < ApplicationRecord
   has_many :conditions_supports, dependent: :destroy
 
-  validates :money, presence: true, uniqueness: true
-  validates :is_myself, presence: true
+  validates :money, uniqueness: true
+  validates :is_myself
   enum is_myself: { user: 0, other: 1 , household: 2 }
 end

44969c32478dcb046f884c76dbbd956625943f40
yutitakagi <takagiyuri0712@gmail.com>
2022-09-16 10:43:02 +0900

    add: income.rbにis_myselfのバリデーションを追加

--- app/tokyoto_app/app/models/income.rb
+++ app/tokyoto_app/app/models/income.rb
@@ -3,6 +3,7 @@ class Income < ApplicationRecord
   has_many :conditions_supports, dependent: :destroy
 
   validates :money, uniqueness: true
+  validates :is_myself, presence: true
 
   enum is_myself: { user: 0, other: 1 , household: 2 }
 end

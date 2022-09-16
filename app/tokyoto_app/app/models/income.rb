5aabfb3089caad5a1280ecef03084703ffead0e8
yutitakagi <takagiyuri0712@gmail.com>
2022-09-16 11:14:01 +0900

    add: income.rbのバリデーションを修正

--- app/tokyoto_app/app/models/income.rb
+++ app/tokyoto_app/app/models/income.rb
@@ -2,7 +2,7 @@
 class Income < ApplicationRecord
   has_many :conditions_supports, dependent: :destroy
 
-  validates :money, presence: true, uniqueness: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :money, uniqueness: true
   validates :is_myself, presence: true
   enum is_myself: { user: 0, other: 1 , household: 2 }
 end

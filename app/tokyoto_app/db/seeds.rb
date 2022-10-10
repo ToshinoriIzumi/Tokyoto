0c4ed67c869a5cf0e5979938236b06ab9a32e5ec
yutitakagi <takagiyuri0712@gmail.com>
2022-10-10 15:57:49 +0900

    add: トランザクションを追加

--- app/tokyoto_app/db/seeds.rb
+++ app/tokyoto_app/db/seeds.rb
@@ -9,13 +9,14 @@ table_names = [
   'hospitals'
 ]
 
-table_names.each do |table_name|
-  path = Rails.root.join("db/seeds/#{Rails.env}/#{table_name}.rb")
+begin
+  table_names.each do |table_name|
+    path = Rails.root.join("db/seeds/#{Rails.env}/#{table_name}.rb")
 
-  if File.exists?(path)
-    puts "Creating #{table_name}.rb..."
-    require path
-  end
+    if File.exists?(path)
+      puts "Creating #{table_name}.rb..."
+      require path
+    end
 
 rescue => e
   puts e.message

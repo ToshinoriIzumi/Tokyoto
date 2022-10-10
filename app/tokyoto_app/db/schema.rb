16542e17bf9e9e84cf2325063a93b8622a492b46
ushijima eisaku <eisakuushijima@gmail.com>
2022-10-10 14:56:34 +0900

    Add_tag_to_supports #46

--- app/tokyoto_app/db/schema.rb
+++ app/tokyoto_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_11_110230) do
+ActiveRecord::Schema.define(version: 2022_10_10_023728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 2022_09_11_110230) do
     t.string "url", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "tag"
   end
 
   create_table "users", force: :cascade do |t|

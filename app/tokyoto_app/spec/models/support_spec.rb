f6d67c758b70b76fb2b4070b0821e9cccdc627ac
yuritakagi <takagiyuri0712@gmail.com>
2022-10-23 10:52:11 +0900

    add: support_specを追記 #53

--- app/tokyoto_app/spec/models/support_spec.rb
+++ app/tokyoto_app/spec/models/support_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Support, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it '支援名、支援内容、url、タグがあれば有効であること' do
+    support = Support.new(
+      support_name: 'support1',
+      content: 'support1_content',
+      url: 'https://www.support1.com',
+      tag: 'tag1',
+    )
+    expect(support).to be_valid
+  end
+
+  it '支援名がなければ無効であること' do
+    support = Support.new(support_name: nil)
+    support.valid?
+    expect(support.errors[:support_name]).to include("can't be blank")
+  end
+
+  it '支援内容がなければ無効であること' do
+    support = Support.new(content: nil)
+    support.valid?
+    expect(support.errors[:content]).to include("can't be blank")
+  end
+
+  it 'urlがなければ無効であること' do
+    support = Support.new(url: nil)
+    support.valid?
+    expect(support.errors[:url]).to include("can't be blank")
+  end
+
+  # urlが重複したら無効にしたい…
+  xit 'urlと支援名が重複していれば無効であること' do
+
+  end
+  # urlのチェックいる？ https~とか、pdfを参考リンクにしたい場合はない？
+
+  it 'タグがなければ無効であること' do
+    support = Support.new(tag: nil)
+    support.valid?
+    expect(support.errors[:tag]).to include("can't be blank")
+  end
 end

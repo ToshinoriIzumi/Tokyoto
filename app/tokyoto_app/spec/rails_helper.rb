dd88ae67e5310861f932c47aaff029a88d2c587a
yuritakagi <takagiyuri0712@gmail.com>
2022-10-26 19:10:57 +0900

    add: simplecovの設定を変更 #53

--- app/tokyoto_app/spec/rails_helper.rb
+++ app/tokyoto_app/spec/rails_helper.rb
@@ -1,3 +1,5 @@
+require 'simplecov'
+SimpleCov.start 'rails'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -5,6 +7,7 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in

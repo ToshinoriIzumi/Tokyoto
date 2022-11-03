747eba778b4a0a8ad4d963046b69835e3d4b3daa
yuritakagi <takagiyuri0712@gmail.com>
2022-11-03 21:09:02 +0900

    add: 各model_specの雛形を形成

--- app/tokyoto_app/spec/models/age_spec.rb
+++ app/tokyoto_app/spec/models/age_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Age, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it '最低年齢、最高年齢があれば有効であること' do
+
+  end
+
+  it '最低年齢がなければ無効であること' do
+
+  end
+
+  it '最高年齢がなければ無効であること' do
+
+  end
+
+  it '最低年齢と最高年齢が一意でなければ無効であること' do
+    
+  end
 end
--- app/tokyoto_app/spec/models/income_spec.rb
+++ app/tokyoto_app/spec/models/income_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Income, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it '金額、本人フラグがあれば有効であること' do
+
+  end
+
+  it '金額がなければ無効であること' do
+
+  end
+
+  it '本人フラグがなければ無効であること' do
+
+  end
+
+  it '金額と本人フラグの組み合わせが一意でなければ無効であること' do
+
+  end
 end

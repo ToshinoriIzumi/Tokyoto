0f3647dcefa9aa092eeefc95444d13cc0829e872
yuritakagi <takagiyuri0712@gmail.com>
2022-11-05 14:52:46 +0900

    add: income_specを追記 #53

--- app/tokyoto_app/spec/models/income_spec.rb
+++ app/tokyoto_app/spec/models/income_spec.rb
@@ -2,18 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Income, type: :model do
   it '金額、本人フラグがあれば有効であること' do
-
+    income = Income.new(money: 2000000, is_myself: 0)
+    expect(income).to be_valid
   end
 
   it '金額がなければ無効であること' do
-
+    income = Income.new(money: nil)
+    income.valid?
+    expect(income.errors[:money]).to include("can't be blank")
+    # 実装不足orテストミス
   end
 
   it '本人フラグがなければ無効であること' do
-
+    income = Income.new(is_myself: nil)
+    income.valid?
+    expect(income.errors[:is_myself]).to include("can't be blank")
   end
 
   it '金額と本人フラグの組み合わせが一意でなければ無効であること' do
-
+    Income.create(money: 2000000, is_myself: 0)
+    income = Income.new(money: 2000000, is_myself: 0)
+    income.valid?
+    expect(income.errors[:money]).to include("has already been taken")
   end
 end

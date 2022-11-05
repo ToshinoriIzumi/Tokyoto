a33db132bfa753a6dd28a2aa85da149f98a7a94a
yuritakagi <takagiyuri0712@gmail.com>
2022-11-05 14:50:26 +0900

    add: status_specを追記 #53

--- app/tokyoto_app/spec/models/status_spec.rb
+++ app/tokyoto_app/spec/models/status_spec.rb
@@ -2,14 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Status, type: :model do
   it '請求者があれば有効であること' do
-
+    status = Status.new(status: 0)
+    expect(status).to be_valid
   end
 
   it '請求者がなければ無効であること' do
-
+    status = Status.new(status: nil)
+    status.valid?
+    expect(status.errors[:status]).to include("can't be blank")
   end
 
   it '請求者が一意でなければ無効であること' do
-    
+    Status.create(status: 0)
+    status = Status.new(status: 0)
+    status.valid?
+    expect(status.errors[:status]).to include("has been taken")
+    # 実装不足orテストミス
   end
 end

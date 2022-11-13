b0f0d22dcef2b2a9effff244f06a9f81691e1347
ushijima eisaku <eisakuushijima@gmail.com>
2022-11-13 15:52:22 +0900

    modelファイルへdependent destroyを追記

--- app/tokyoto_app/app/models/support.rb
+++ app/tokyoto_app/app/models/support.rb
@@ -1,6 +1,6 @@
 class Support < ApplicationRecord
   has_many :conditions_supports, dependent: :destroy
-  has_many :support_tags
+  has_many :support_tags, dependent: :destroy
   has_many :tags, through: :support_tags
 
   validates :support_name, presence: true
--- app/tokyoto_app/app/models/tag.rb
+++ app/tokyoto_app/app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
-  has_many :support_tags
+  has_many :support_tags, dependent: :destroy
   has_many :supports, through: :support_tags
 
   validates :name, presence: true

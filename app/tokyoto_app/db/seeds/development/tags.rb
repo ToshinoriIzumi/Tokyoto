b8b5d5fde51ea02de7516d63bf0a22d8f0090841
ushijima eisaku <eisakuushijima@gmail.com>
2022-11-19 13:13:03 +0900

    Fix : seed「tags.rb」の修正  #46

--- app/tokyoto_app/db/seeds/development/tags.rb
+++ app/tokyoto_app/db/seeds/development/tags.rb
@@ -1,12 +1,12 @@
 Tag.create!(
   [
-    { name: '医療', color: 'orange' },
-    { name: '教育', color: 'teal' },
-    { name: '住宅', color: 'blue' },
-    { name: '育児', color: 'yellow' },
-    { name: '貸付金', color: 'gray' },
-    { name: '給付金', color: 'gray' },
-    { name: '就業支援', color: 'gray' },
-    { name: '負担金助成', color: 'gray' },
+    { name: '医療'},
+    { name: '教育'},
+    { name: '住宅'},
+    { name: '育児'},
+    { name: '貸付金'},
+    { name: '給付金'},
+    { name: '就業支援'},
+    { name: '負担金助成'},
   ]
 )

63707095178328e9f5f468ca2517f473f6f874c4
ushijima eisaku <eisakuushijima@gmail.com>
2022-11-19 11:54:09 +0900

    Fix : gemdraperの導入、tags tableのcolorカラムの削除、tag_decoratorの作成・編集  #46

--- app/tokyoto_app/db/schema.rb
+++ app/tokyoto_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_12_101641) do
+ActiveRecord::Schema.define(version: 2022_11_19_024955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,7 +108,6 @@ ActiveRecord::Schema.define(version: 2022_11_12_101641) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name", null: false
-    t.string "color"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

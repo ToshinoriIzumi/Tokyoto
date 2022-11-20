8a5f19f3b2656b2af7854fd660313340c95e7f83
yuritakagi <takagiyuri0712@gmail.com>
2022-11-20 14:08:16 +0900

    add: プロフィール登録を推奨するフラッシュメッセージの表示を修正

--- app/tokyoto_app/app/controllers/top_controller.rb
+++ app/tokyoto_app/app/controllers/top_controller.rb
@@ -1,6 +1,5 @@
 class TopController < ApplicationController
   before_action :set_q, only: [:index, :search]
-  before_action :info_profile, only: [:index]
   skip_before_action :require_login
 
   def index
@@ -18,9 +17,5 @@ class TopController < ApplicationController
   private
   def set_q
     @q = ConditionsSupport.ransack(params[:q])
-  end
-
-  def info_profile
-    flash.now[:info] = '<a href="profile/edit">プロフィール</a>を登録すると、より便利な機能が使えます'.html_safe unless current_user.user_name
   end
 end

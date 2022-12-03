58ed83836d79a584191b270773ac5094f1e9989c
kashiwagireinanoMacBook-Air <azisai07@gmail.com>
2022-12-03 12:22:43 +0900

    base_controllerにcheck_admin追加
    弾かれた場合の遷移先は、一般の一覧画面がいいと思うrootにしてあります。

--- app/tokyoto_app/app/controllers/municipality/base_controller.rb
+++ app/tokyoto_app/app/controllers/municipality/base_controller.rb
@@ -7,4 +7,8 @@ class Municipality::BaseController < ApplicationController
       flash[:info] = 'ログインしてください'
       redirect_to main_app.login_path
     end
+
+    def check_admin
+      redirect_to root_path, warning: '権限がありません' unless current_user.admin?
+    end
 end

9b0093e4644ebbc3a19af7eedcbc0df7c34008a6
yuki_sdy <couragewing.14@gmail.com>
2022-12-11 12:45:39 +0900

    viewとDB間で変換を行うメソッドを作成 #91

--- app/tokyoto_app/app/controllers/application_controller.rb
+++ app/tokyoto_app/app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
 
+  def income_to_db(param)
+    param.to_i * 10000
+  end
+
+  def income_to_view(data)
+    data.to_i / 10000
+  end
+
   private
-    def not_authenticated
-      flash[:info] = 'ログインしてください'
-      redirect_to main_app.login_path
-    end
+
+  def not_authenticated
+    flash[:info] = 'ログインしてください'
+    redirect_to main_app.login_path
+  end 
 end

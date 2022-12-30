2156e051710d8d0f071517cbb789b6e7f1f4d04e
yuritakagi <takagiyuri0712@gmail.com>
2022-12-30 09:36:50 +0900

    add: フォームオブジェクトを使ったプロフィール登録・編集機能の実装

--- app/tokyoto_app/app/forms/profile_form.rb
+++ app/tokyoto_app/app/forms/profile_form.rb
@@ -11,6 +11,7 @@ class ProfileForm
   # バリデーション他に付けたほうが良いものあるか？
   validates :city_id, presence: true
   validates :income, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   #persisted?は作成・更新に応じてフォームのアクションをPOST・PATCHに切り替えてくれる
   # delegate :persisted?, to: :profile
@@ -22,7 +23,7 @@ class ProfileForm
     if user.children.present?
       Child.where(user_id: id).update(age: age)
     else
-      Child.create!(age: age, user_id: id)
+      Child.create(age: age, user_id: id)
     end
   end
 

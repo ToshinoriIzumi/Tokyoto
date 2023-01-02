0fa4d555756d0c4ef2f4212382a0f4a04f09f20c
kuuwannya <kuuuuu05062525@gmail.com>
2023-01-02 19:51:58 +0900

    fix unique制約を削除 #64

---
+++ app/tokyoto_app/db/migrate/20230102100226_create_conditions_supports.rb
@@ -0,0 +1,18 @@
+class CreateConditionsSupports < ActiveRecord::Migration[6.0]
+  def change
+    create_table :conditions_supports, id: false do |t|
+      t.references :condition, null: false, foreign_key: true
+      t.references :support, null: false, foreign_key: true
+      t.references :city, null: false, foreign_key: true
+      t.integer :dependents_num, null: false
+      t.integer :payment, null: false
+      t.references :income, null: false, foreign_key: true
+      t.references :age, null: false, foreign_key: true
+
+      t.timestamps
+    end
+
+    sql = 'ALTER TABLE conditions_supports ADD PRIMARY KEY (condition_id, support_id);'
+    ActiveRecord::Base.connection.execute(sql)
+  end
+end

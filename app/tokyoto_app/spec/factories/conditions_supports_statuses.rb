87b9483828cb6aa38d62ec839bd21fbc1dfaac01
yuki_sdy <couragewing.14@gmail.com>
2023-01-10 15:45:24 +0900

    conditions_support関連データの修正 #95

--- app/tokyoto_app/spec/factories/conditions_supports_statuses.rb
+++ app/tokyoto_app/spec/factories/conditions_supports_statuses.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :conditions_supports_status do
-    
+    association :condition
+    association :status    
   end
 end
--- app/tokyoto_app/spec/models/conditions_supports_status_spec.rb
+++ app/tokyoto_app/spec/models/conditions_supports_status_spec.rb
@@ -1,5 +1,30 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe ConditionsSupportsStatus, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe ConditionsSupportsStatus, type: :model do
+  before do
+    conditions_supports_status = ConditionsSupportsStatus.new(
+      condition_id: 1
+      support_id: 1
+    )
+  end
+
+  describe 'validation' do
+    it "condition_idとsupport_idがある場合、有効であること" do
+      expect(conditions_supports_status).to be_valid
+    end
+  
+    it "condition_idがない場合、無効であること" do
+      conditions_supports_status.condition_id = nil
+      expect(conditions_supports_status).to be_invalid
+      # conditions_supports_status.valid?
+      expect(condition_id.errors[:conditions_supports_status]).to include("can't be blank")
+    end
+    
+    it "support_idがない場合、無効であること" do
+      conditions_supports_status.support_id = nil
+      expect(conditions_supports_status).to be_invalid
+      # conditions_supports_status.valid?
+      expect(support_id.errors[:conditions_supports_status]).to include("can't be blank")
+    end
+  end
+end

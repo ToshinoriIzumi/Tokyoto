6cd406ddda46914f53ffbd7da386d93d93518601
yuki_sdy <couragewing.14@gmail.com>
2023-01-11 13:25:26 +0900

    condition_supportsテスト不足情報追記 #95

--- app/tokyoto_app/spec/models/conditions_supports_status_spec.rb
+++ app/tokyoto_app/spec/models/conditions_supports_status_spec.rb
@@ -2,9 +2,15 @@ require 'rails_helper'
 
 RSpec.describe ConditionsSupportsStatus, type: :model do
   before do
+    condition = Condition.new
+    support = Support.new(
+      support_name: 'support1',
+      content: 'support1_content',
+      url: 'https://www.support1.com',
+      )
     conditions_supports_status = ConditionsSupportsStatus.new(
-      condition_id: 1
-      support_id: 1
+      condition_id: condition.id
+      support_id: support.id
     )
   end
 
@@ -15,16 +21,14 @@ RSpec.describe ConditionsSupportsStatus, type: :model do
   
     it "condition_idがない場合、無効であること" do
       conditions_supports_status.condition_id = nil
-      expect(conditions_supports_status).to be_invalid
-      # conditions_supports_status.valid?
-      expect(condition_id.errors[:conditions_supports_status]).to include("can't be blank")
+      conditions_supports_status.valid?
+      expect(conditions_supports_status.errors[:condition_id]).to include("can't be blank")
     end
     
     it "support_idがない場合、無効であること" do
       conditions_supports_status.support_id = nil
-      expect(conditions_supports_status).to be_invalid
-      # conditions_supports_status.valid?
-      expect(support_id.errors[:conditions_supports_status]).to include("can't be blank")
+      conditions_supports_status.valid?
+      expect(conditions_supports_status.errors[:support_id]).to include("can't be blank")
     end
   end
 end

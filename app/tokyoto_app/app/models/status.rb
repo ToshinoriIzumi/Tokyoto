b8f23122a214c1d91bbed41387fcb3344026f203
kuuwannya <kuuuuu05062525@gmail.com>
2023-01-15 15:26:52 +0900

    ConditionsSupportsStatusesテーブルのhas_many throughの追加 #64

--- app/tokyoto_app/app/models/status.rb
+++ app/tokyoto_app/app/models/status.rb
@@ -1,5 +1,6 @@
 class Status < ApplicationRecord
   has_many :conditions_supports_statuses, dependent: :destroy
+  has_many :conditions, through: :conditions_supports_statuses
   validates :status, presence: true, uniqueness: true
 
 end

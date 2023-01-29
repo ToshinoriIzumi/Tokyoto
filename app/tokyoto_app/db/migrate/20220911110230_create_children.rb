86e97e0086a38fb6b9270c89f6d16c20f4b2f0ef
kuuwannya <kuuuuu05062525@gmail.com>
2023-01-30 00:39:10 +0900

    fix Childrenテーブルのageカラムをdate型へ変更 #105

--- app/tokyoto_app/db/migrate/20220911110230_create_children.rb
+++ app/tokyoto_app/db/migrate/20220911110230_create_children.rb
@@ -2,7 +2,7 @@ class CreateChildren < ActiveRecord::Migration[6.0]
   def change
     create_table :children do |t|
       t.references :user, null: false, foreign_key: true
-      t.integer :age
+      t.date :age
 
       t.timestamps
     end

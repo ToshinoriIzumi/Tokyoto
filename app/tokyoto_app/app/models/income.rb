b3217cc0f49fae3dda88c2eaf056ba66dbc9dd96
kuuwannya <kuuuuu05062525@gmail.com>
2023-01-30 23:49:26 +0900

    fix Incomesテーブルのis_myselfカラムのenumを修正 #105

--- app/tokyoto_app/app/models/income.rb
+++ app/tokyoto_app/app/models/income.rb
@@ -4,8 +4,8 @@ class Income < ApplicationRecord
   has_many :conditions_supports_incomes, dependent: :destroy
   has_many :conditions_supports, through: :conditions_supports_incomes, primary_key: [:condition_id, :support_id]
 
-  validates :money, uniqueness: { scope: [:is_myself] }
+  validates :money, uniqueness: { scope: [:is_myself] }, presence: true
   validates :is_myself, presence: true
 
-  enum is_myself: { user: 0, other: 1 , household: 2 }
+  enum is_myself: { user: 0, household: 1, spouse_and_support_obligor: 2, business_income: 3 }
 end

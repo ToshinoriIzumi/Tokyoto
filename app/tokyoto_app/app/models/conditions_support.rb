1da676b3caf69b76219394d503b325a73c8a8f6b
kuuwannya <kuuuuu05062525@gmail.com>
2023-02-02 22:45:01 +0900

    fix ConditionsSupportのテーブルの修正 #105

--- app/tokyoto_app/app/models/conditions_support.rb
+++ app/tokyoto_app/app/models/conditions_support.rb
@@ -1,7 +1,6 @@
 class ConditionsSupport < ApplicationRecord
   belongs_to :support
   belongs_to :city
-  belongs_to :income
   belongs_to :age
   belongs_to :condition
   has_many :conditions_supports_incomes, dependent: :destroy
@@ -12,10 +11,8 @@ class ConditionsSupport < ApplicationRecord
   validates :payment_limit, presence: true
   validates :payment_frequency, presence: true
 
-  enum :payment_limit { fixed_amount: 0, monthly_limit: 1 , upper_limit: 2, full_payment: 3 }
-  enum :payment_frequency { lump: 0, every_one_month: 1 , every_two_months: 2, every_three_months: 3, every_four_months: 4 }
-
-  self.primary_key = :condition_id, :support_id
+  enum payment_limit: { fixed_amount: 0, monthly_limit: 1 , upper_limit: 2, full_payment: 3 }
+  enum payment_frequency: { lump: 0, every_one_month: 1 , every_two_months: 2, every_three_months: 3, every_four_months: 4 }
 
   scope :age_search, -> (number){ joins(:age).where("min <= ? and ? <= max", number, number)}
 
--- app/tokyoto_app/db/migrate/20230102100226_create_conditions_supports.rb
+++ app/tokyoto_app/db/migrate/20230102100226_create_conditions_supports.rb
@@ -1,11 +1,10 @@
 class CreateConditionsSupports < ActiveRecord::Migration[6.0]
   def change
-    create_table :conditions_supports, id: false do |t|
+    create_table :conditions_supports do |t|
       t.references :condition, null: false, foreign_key: true
       t.references :support, null: false, foreign_key: true
       t.references :city, null: false, foreign_key: true
-      t.string :payment, null: false
-      t.references :income, null: false, foreign_key: true
+      t.integer :payment, null: false
       t.references :age, null: false, foreign_key: true
       t.string :url, null: false, index: {unique: true}
       t.integer :payment_limit, default: 0
@@ -15,8 +14,5 @@ class CreateConditionsSupports < ActiveRecord::Migration[6.0]
 
       t.timestamps
     end
-
-    sql = 'ALTER TABLE conditions_supports ADD PRIMARY KEY (condition_id, support_id);'
-    ActiveRecord::Base.connection.execute(sql)
   end
 end

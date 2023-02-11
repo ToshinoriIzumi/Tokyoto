58f23c02396dd528b246b82fd1cb34686a1e9fa0
kuuwannya <kuuuuu05062525@gmail.com>
2023-02-11 12:50:54 +0900

    fix プロフィールの登録フォームの修正 #116

--- app/tokyoto_app/app/controllers/profiles_controller.rb
+++ app/tokyoto_app/app/controllers/profiles_controller.rb
@@ -8,6 +8,7 @@ class ProfilesController < ApplicationController
   def create
     @profile_form = ProfileForm.new(profile_params)
     @profile_form.id = current_user.id
+    binding.pry
     if @profile_form.save
       redirect_to root_path, notice: 'プロフィールを登録しました。'
     else
@@ -24,7 +25,7 @@ class ProfilesController < ApplicationController
 
   def edit
     user = current_user
-    @profile_form = ProfileForm.new(id: user.id, user_name: user.user_name, city_id: user.city_id, income: user.income, age: user.children[0].age)
+    @profile_form = ProfileForm.new(id: user.id, city_id: user.city_id, income: user.income, birth: user.children[0].birth)
   end
 
   def update
@@ -41,9 +42,10 @@ class ProfilesController < ApplicationController
   private
 
   def profile_params
-    params.require(:profile_form).permit(:user_name, :city_id, :income, :age)
+    params.require(:profile_form).permit(:city_id, :income, :birth)
   end
 
   def set_cities
     @cities = City.all
+  end
 end

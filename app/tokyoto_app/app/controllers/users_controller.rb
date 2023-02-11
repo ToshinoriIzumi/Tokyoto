64e066ea4723bdb451244808dec6e0c5a1bc6169
kuuwannya <kuuuuu05062525@gmail.com>
2023-02-11 12:44:44 +0900

    fix ユーザー登録にuser_nameを追加 #105

--- app/tokyoto_app/app/controllers/users_controller.rb
+++ app/tokyoto_app/app/controllers/users_controller.rb
@@ -26,6 +26,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :user_name)
   end
 end

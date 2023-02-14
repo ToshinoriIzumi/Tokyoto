e9dcc6d61f8f2c99eb4d6ce34f0783a9bf349a48
yuki_sdy <couragewing.14@gmail.com>
2023-02-14 15:50:14 +0900

    tag関係のテストを更新 #95

--- app/tokyoto_app/spec/models/supports_tag_spec.rb
+++ app/tokyoto_app/spec/models/supports_tag_spec.rb
@@ -1,32 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe SupportsTag, type: :model do
+RSpec.describe SupportTag, type: :model do
   before do
-    tag = Tag.new(name: 'tag_name')
-    support = Support.new(
+    tag = Tag.create(name: 'tag_name')
+    support = Support.create(
       support_name: 'support1',
       content: 'support1_content',
-      url: 'https://www.support1.com',
+      user_application_method: 'support1_user_application_method',
+      user_application_limit: '記載なし',
+      user_renewal_method: '記載なし',
+      user_renewal_month: '記載なし',
+      publish_state: 0,
       )
-    supports_tag = SupportsTag.new(
-      support_id: support.id
+    @support_tag = SupportTag.new(
+      support_id: support.id,
       tag_id: tag.id
       )
   end
 
   it 'support_idとtag_idがある場合、有効であること' do
-    expect(supports_tag).to be_valid
+    expect(@support_tag).to be_valid
   end
 
   it 'support_idがない場合、無効であること' do
-    supports_tag.support_id = nil
-    supports_tag.valid?
-    expect(supports_tag.errors[:support_id]).to include("can't be blank")
+    @support_tag.support_id = nil
+    @support_tag.valid?
+    expect(@support_tag.errors[:support]).to include("を入力してください")
   end
 
   it 'tag_idがない場合、無効であること' do
-    supports_tag.tag_id = nil
-    supports_tag.valid?
-    expect(supports_tag.errors[:tag_id]).to include("can't be blank")
+    @support_tag.tag_id = nil
+    @support_tag.valid?
+    expect(@support_tag.errors[:tag]).to include("を入力してください")
   end
 end
--- app/tokyoto_app/spec/models/tag_spec.rb
+++ app/tokyoto_app/spec/models/tag_spec.rb
@@ -1,22 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  before { tag = Tag.new(name: 'tag_name') }
+  before do
+    @tag = Tag.new(name: 'tag_name')
+  end
 
   it 'タグの名前があれば有効であること' do
-    expect(tag).to be_valid
+    expect(@tag).to be_valid
   end
 
   it 'タグの名前がなければ無効であること' do
-    tag.name = nil
-    tag.valid?
-    expect(tag.errors[:name]).to include("can't be blank")
+    @tag.name = nil
+    @tag.valid?
+    expect(@tag.errors[:name]).to include("を入力してください")
   end
 
   it 'タグの名前が一意でなければ無効であること' do
-    tag.save
+    @tag.save
     tag_with_same_name = Tag.new(name: 'tag_name')
     tag_with_same_name.valid?
-    expect(tag_with_same_name.errors[:name]).to include("has already been taken")
+    expect(tag_with_same_name.errors[:name]).to include("はすでに存在します")
   end
 end

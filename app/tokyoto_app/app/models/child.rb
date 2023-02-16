3e64d545c1d15667fa64edf2c09e2a0225e25d3a
yuki_sdy <couragewing.14@gmail.com>
2023-02-16 17:09:45 +0900

    子供の生年月日を年齢に変換するメソッドを追記 #120

--- app/tokyoto_app/app/models/child.rb
+++ app/tokyoto_app/app/models/child.rb
@@ -2,4 +2,8 @@ class Child < ApplicationRecord
   belongs_to :user
 
   validates :birth, presence: true
+
+  def age(date)
+    (Date.today.strftime('%Y%m%d').to_i - date.strftime('%Y%m%d').to_i) / 10000
+  end
 end

2d3e7d0a5a2f9541dd01bc028726b365581c919e
kashiwagireinanoMacBook-Air <azisai07@gmail.com>
2023-02-17 15:29:21 +0900

    idの追加#124

--- app/tokyoto_app/db/seeds/development/conditions_supports_income.rb
+++ app/tokyoto_app/db/seeds/development/conditions_supports_income.rb
@@ -1,6 +1,7 @@
 ConditionsSupportsIncome.create!(
   [
     {
+      id: 1
       conditions_support_id: 1,
       income_id: 1
     },
--- app/tokyoto_app/db/seeds/development/conditions_supports_statuses.rb
+++ app/tokyoto_app/db/seeds/development/conditions_supports_statuses.rb
@@ -6,22 +6,27 @@ ConditionsSupportsStatus.create!(
       status_id: 1
     },
     {
+      id: 2
       condition_id: 1,
       status_id: 2
     },
     {
+      id: 3
       condition_id: 1,
       status_id: 3
     },
     {
+      id: 4
       condition_id: 1,
       status_id: 4
     },
     {
+      id: 5
       condition_id: 1,
       status_id: 5
     },
     {
+      id: 6
       condition_id: 1,
       status_id: 6
     },

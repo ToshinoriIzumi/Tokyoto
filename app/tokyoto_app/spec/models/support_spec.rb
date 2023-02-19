629b916341745ed7620549408ae4527ed24d1332
yuki_sdy <couragewing.14@gmail.com>
2023-02-19 11:55:10 +0900

    supportsモデルのスコープテスト枠だけ追加 #95

--- app/tokyoto_app/spec/models/support_spec.rb
+++ app/tokyoto_app/spec/models/support_spec.rb
@@ -1,39 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Support, type: :model do
-  before do
-    @support = Support.new(
-      support_name: 'support1',
-      content: 'support1_content',
-      user_application_method: 'support1_user_application_method',
-      user_application_limit: '記載なし',
-      user_renewal_method: '記載なし',
-      user_renewal_month: '記載なし',
-      publish_state: 0,
-      )
+  describe 'validation' do
+    it '全てのカラムに情報があれば有効であること' do
+      support = build(:support)
+      expect(support).to be_valid
+    end
+    
+    it '支援名がなければ無効であること' do
+      support = build(:support, support_name: nil)
+      support.valid?
+      expect(support.errors[:support_name]).to include("を入力してください")
+    end
+    
+    it '支援内容がなければ無効であること' do
+      support = build(:support, content: nil)
+      support.valid?
+      expect(support.errors[:content]).to include("を入力してください")
+    end
+    
+    it '支援名が一意でなければ無効であること' do
+      support = create(:support)
+      another_support = build(:support, support_name: support.support_name)
+      another_support.valid?
+      expect(another_support.errors[:support_name]).to include("はすでに存在します")
+    end
   end
-
-  it '全てのカラムに情報があれば有効であること' do
-    support = build(:support)
-    expect(support).to be_valid
-  end
-  
-  it '支援名がなければ無効であること' do
-    support = build(:support, support_name: nil)
-    support.valid?
-    expect(support.errors[:support_name]).to include("を入力してください")
-  end
-  
-  it '支援内容がなければ無効であること' do
-    support = build(:support, content: nil)
-    support.valid?
-    expect(support.errors[:content]).to include("を入力してください")
-  end
-  
-  it '支援名が一意でなければ無効であること' do
-    support = create(:support)
-    another_support = build(:support, support_name: support.support_name)
-    another_support.valid?
-    expect(another_support.errors[:support_name]).to include("はすでに存在します")
+  describe 'scope' do
+    xit 'by_tag' do
+    end
+    xit 'support_name_contain' do
+    end
+    xit 'state_contain' do
+    end
   end
 end

e7eb04181d7084f9693985ed6ada39696e1ae7ec
kuuwannya <kuuuuu05062525@gmail.com>
2023-02-19 23:59:34 +0900

    add カラムの追加に伴うmodelの追加 #129

--- app/tokyoto_app/app/models/child.rb
+++ app/tokyoto_app/app/models/child.rb
@@ -2,4 +2,6 @@ class Child < ApplicationRecord
   belongs_to :user
 
   validates :birth, presence: true
+
+  enum sibling_conposition: { first_child: 0, second_child: 1 , third_child_and_above: 2 }
 end

d2c283ed42b89bf75c93d0ac9a25fd8cd3d7e4d2
yuki_sdy <couragewing.14@gmail.com>
2023-02-27 16:20:13 +0900

    showやeditのviewへstatus情報を追加 #135

--- app/tokyoto_app/app/controllers/profiles_controller.rb
+++ app/tokyoto_app/app/controllers/profiles_controller.rb
@@ -22,10 +22,18 @@ class ProfilesController < ApplicationController
     @user = User.find(current_user.id)
     @profile_form = ProfileForm.new(id: @user.id)
     @city = City.find(current_user.city_id)
+    @statuses = UserStatus.where(user_id: @user.id).pluck(:is_status).map{|s| Status.find(s)}
   end
-
+  
   def edit
     user = current_user
+    statuses_id = UserStatus.where(user_id: user.id).pluck(:is_status)
+    statuses_id.each do |id|
+      change_welfare(id)
+    end
+    statuses_id.each do |id|
+      change_dependents(id)
+    end
     @births = []
     user.children.each do |child|
       @births.push(child.birth)
@@ -35,7 +43,9 @@ class ProfilesController < ApplicationController
       id: user.id,
       city_id: user.city_id,
       income: user.income,
-      births: @births
+      births: @births,
+      is_welfare: @welfare,
+      number_of_dependents: @dependents,
     )
   end
 
@@ -53,7 +63,7 @@ class ProfilesController < ApplicationController
   private
 
   def profile_params
-    params.require(:profile_form).permit(:city_id, :income, { births: [] })
+    params.require(:profile_form).permit(:city_id, :income, { births: [] }, :is_welfare, :number_of_dependents)
   end
 
   def set_cities
@@ -62,5 +72,23 @@ class ProfilesController < ApplicationController
 
   def set_births
     @births = profile_params[:births]
+  end
+
+  def change_welfare(id)
+    if id === 5
+      @welfare = true
+    elsif id === 18
+      @welfare = false
+    end
+  end
+
+  def change_dependents(id)
+    if id === 19
+      @dependents = 'one'
+    elsif id === 20
+      @dependents = 'two'
+    elsif id === 21
+      @dependents = 'three_than'
+    end
   end
 end

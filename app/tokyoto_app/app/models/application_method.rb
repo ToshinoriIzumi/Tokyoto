b5be9fc8a725f9decc0da94b9f2b361678ca7c09
kuuwannya <kuuuuu05062525@gmail.com>
2023-03-11 21:59:29 +0900

    fix seedデータの修正 #129

--- app/tokyoto_app/app/models/application_method.rb
+++ app/tokyoto_app/app/models/application_method.rb
@@ -3,5 +3,5 @@ class ApplicationMethod < ApplicationRecord
   has_many :conditions_supports, through: :conditions_supports_applications_methods
 
   validates :application_method, presence: true
-  enum method_order: { first: 0, second: 1, third: 2, forth: 3, fifth: 4 }
+  enum method_order: { first_method: 0, second_methods: 1, third_methods: 2, forth_methods: 3, fifth_methods: 4 }
 end

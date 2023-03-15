28e851c0b2bed46d9c4a921710c6c180b42ded97
tsukudakouhei <tsuku.pique.job@gmail.com>
2023-03-15 19:49:39 +0900

    Fix: subメソッドでnilケースを考慮し修正

--- app/tokyoto_app/app/helpers/conditions_support_show_helper.rb
+++ app/tokyoto_app/app/helpers/conditions_support_show_helper.rb
@@ -1,10 +1,10 @@
 module ConditionsSupportShowHelper
 
   def replace_head_of_line_to_interpoint(data)
-    data.sub(/^/, '・')
+    data.sub(/^/, '・') if data.present?
   end
 
   def replace_comma_to_interpoint(data)
-    data.gsub(/,/, '・')
+    data.gsub(/,/, '・') if data.present?
   end
 end
